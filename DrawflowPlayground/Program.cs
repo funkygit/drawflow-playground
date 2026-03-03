ab8d6644b9962ca1eb86940e5d145e9fa9cc0686
funkygit <funkyabhishek1@gmail.com>
2026-03-03 23:59:52 +0530

    Project restructuring

--- DrawflowPlayground/Program.cs
+++ DrawflowPlayground/Program.cs
@@ -19,12 +19,12 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-//app.MapStaticAssets();
+app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-    //.WithStaticAssets();
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
 
 app.MapHub<DrawflowPlayground.Hubs.WorkflowHub>("/workflowHub");
 
